a9443a910a5462bf77abd731cabf6d1c39fa2c02
Vansh Kashyap <145321719+Vansh-kash2023@users.noreply.github.com>
2025-02-18 21:05:33 +0530

    renamed chatbot

--- app/screens/chatbot.js
+++ app/screens/chatbot.js
@@ -61,7 +61,7 @@ const ChatbotScreen = () => {
             <View className="flex-1 bg-white px-6 py-4 mt-20">
                 
                 {/* Heading */}
-                <Text className="text-4xl font-bold text-gray-800 mb-4 self-center">AI Chatbot</Text>
+                <Text className="text-4xl font-bold text-gray-800 mb-4 self-center">Care Taker Asisstant</Text>
                 
                 {/* Chat Window */}
                 <View className="flex-1 bg-gray-100 p-4 rounded-2xl mb-4">
